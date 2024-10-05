e34d632e0c4255493bf16069ec97cb4b1e96cceb
x20888450 <x20888450@gmail.com>
2024-10-05 19:36:24 +0800

    update

--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -1,19 +1,8 @@
-#include <cstring>
-#include <thread>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
 #include <unistd.h>
-#include <cinttypes>
-#include "hack.h"
-#include "zygisk.hpp"
-#include "game.h"
-#include "log.h"
-
-using zygisk::Api;
-using zygisk::AppSpecializeArgs;
-using zygisk::ServerSpecializeArgs;
+#include <sys/stat.h>
 
 class MyModule : public zygisk::ModuleBase {
 public:
@@ -31,50 +20,25 @@ public:
     }
 
     void postAppSpecialize(const AppSpecializeArgs *) override {
-        if (enable_hack) {
-            std::thread hack_thread(hack_prepare, game_data_dir, data, length);
-            hack_thread.detach();
-        }
-    }
+        // 获取当前进程 ID
+        pid_t pid = getpid();
 
-private:
-    Api *api;
-    JNIEnv *env;
-    bool enable_hack;
-    char *game_data_dir;
-    void *data;
-    size_t length;
+        // 构建文件路径
+        std::ostringstream filepath;
+        filepath << "/sdcard/env/" << pid << "_env.log";
 
-    void preSpecialize(const char *package_name, const char *app_data_dir) {
-        if (strcmp(package_name, GamePackageName) == 0) {
-            LOGI("detect game: %s", package_name);
-            enable_hack = true;
-            game_data_dir = new char[strlen(app_data_dir) + 1];
-            strcpy(game_data_dir, app_data_dir);
+        // 创建目录（如果不存在）
+        mkdir("/sdcard/env", 0755);
 
-#if defined(__i386__)
-            auto path = "zygisk/armeabi-v7a.so";
-#endif
-#if defined(__x86_64__)
-            auto path = "zygisk/arm64-v8a.so";
-#endif
-#if defined(__i386__) || defined(__x86_64__)
-            int dirfd = api->getModuleDir();
-            int fd = openat(dirfd, path, O_RDONLY);
-            if (fd != -1) {
-                struct stat sb{};
-                fstat(fd, &sb);
-                length = sb.st_size;
-                data = mmap(nullptr, length, PROT_READ, MAP_PRIVATE, fd, 0);
-                close(fd);
-            } else {
-                LOGW("Unable to open arm file");
+        // 打开文件
+        std::ofstream log_file(filepath.str(), std::ios::out);
+
+        if (log_file.is_open()) {
+            // 遍历环境变量
+            for (char **env = environ; *env != nullptr; ++env) {
+                log_file << *env << std::endl;
             }
-#endif
-        } else {
-            api->setOption(zygisk::Option::DLCLOSE_MODULE_LIBRARY);
+            log_file.close();
         }
     }
 };
-
-REGISTER_ZYGISK_MODULE(MyModule)
